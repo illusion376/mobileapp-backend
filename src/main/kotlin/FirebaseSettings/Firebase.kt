b13d50384132e969c39834e5b370ac827ae280e7
dmb17 <dmb17@tpu.ru>
2026-04-23 19:11:03 +0700

    Main files for Firebase - email verf

---
+++ src/main/kotlin/FirebaseSettings/Firebase.kt
@@ -0,0 +1,24 @@
+package io.illusion.FirebaseSettings
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import io.ktor.server.application.*
+
+object FirebaseConfig {
+    const val WEB_API_KEY = "AIzaSyD2oaXud9EbB4uwhu5AesX5e27RVnJYEIA"
+    const val AUTH_REST_URL = "https://identitytoolkit.googleapis.com/v1/accounts"
+
+    fun init() {
+        val serviceAccount = object {}.javaClass.classLoader.getResourceAsStream("firebase-key.json")
+            ?: throw Exception("Файл firebase-key.json не найден в src/main/resources")
+
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build()
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options)
+        }
+    }
+}
---
+++ src/main/kotlin/FirebaseSettings/FirebaseClient.kt
@@ -0,0 +1,15 @@
+package io.illusion.FirebaseSettings
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+object FirebaseClient {
+    val httpClient = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json { ignoreUnknownKeys = true })
+        }
+    }
+}
