0e922971f8cd8d6694478d156a6fc289b3dc8649
dmb17 <dmb17@tpu.ru>
2026-04-23 19:18:59 +0700

    Changes in main files for verf email and logic reg or login

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,19 +4,20 @@ import features.register.configureRegisterRouter
 import features.login.configureLoginRouter
 import io.illusion.data.database.DatabaseFactory
 import io.illusion.data.database.initDatabase
+import io.illusion.FirebaseSettings.FirebaseConfig
 import io.illusion.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 
 fun main() {
-    embeddedServer(CIO, port = 8080, host = "185.244.51.59", module = Application::module)
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
-
 fun Application.module() {
     initDatabase()
+    FirebaseConfig.init()
     configureSerialization()
     configureRouting()
     configureLoginRouter()
