0e7751739bd2b11e4f7f80b1c1676c5c68155ecf
Ersunovich <xaxa_xexe_99@mail.ru>
2026-05-24 21:42:02 +0700

    DB_update

--- src/main/kotlin/data/database/tables/Users.kt
+++ src/main/kotlin/data/database/tables/Users.kt
@@ -5,13 +5,10 @@ import org.jetbrains.exposed.sql.javatime.timestamp
 import org.jetbrains.exposed.sql.javatime.CurrentTimestamp
 
 object Users : Table() {
-    val id = uuid("id")
+    val id = integer("id").autoIncrement()
     val email = varchar("email", 255).uniqueIndex()
     val login = varchar("login", 255)
     val passwordHash = varchar("password", 255)
-    val username = varchar("username", 255)
-    val streak = integer("streak").default(0)
-    val difficulty = double("difficulty").default(1.0)
     val lastActiveDate = timestamp("last_active_date").nullable()
     val createdAccountTime = timestamp("created_account_time").defaultExpression(CurrentTimestamp())
 
